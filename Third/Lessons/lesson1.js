d5e306eac7d33c4f63d71c96e5d31678ce9cb5b1
ZENTRIXXX <azizbee353@gmail.com>
2026-02-28 11:25:14 +0500

    first

--- Third/Lessons/lesson1.js
+++ Third/Lessons/lesson1.js
@@ -1,5 +1,20 @@
-console.log("Salom")
+// console.log("Salom");
 
-// review github
+// Rewiew Github 
 
-// 1.github da repository  
+// 1.Githubda Repository Yaratdik.
+// 2.git init - local dagi papkani github ga bog'lash uchun ko'prik vazifasini bjaradi.
+// 3.git remote add origin "repoz-link" - bog'lashning 2-jarayoni
+
+//4.git add . - localdagi codelaeni git dasturiga qo;shib beradi 
+// 5.git commit -m "comment yozish" - git dasturiga tushgan code larga izoh yozib beradi.
+// 6. git push origin master - git dasturiga code larni GitHub ga yuklab beradi .
+
+// ===================================
+
+// VS code ni GitHub ga bog'lash uchun 2ta code yozish kerak.
+
+// Bu jarayonni komentariya yozayotgan chiqadi shunda ishlatasiz .
+
+// 1.git config --global user.email "eshmat@gmail.com"
+// 2.git config --global user.name "github name"
