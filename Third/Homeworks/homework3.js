5e764f587ba99a02d35485e3d10f2df124e7fa71
ZENTRIXXX <azizbee353@gmail.com>
2026-03-12 10:35:21 +0500

    homework 3

--- Third/Homeworks/homework3.js
+++ Third/Homeworks/homework3.js
@@ -23,3 +23,19 @@ let haftaKuni = haftalar[date.getDay()];
 console.log(
   `Hozir: ${soat}:${minut}:${sekund} | Sana: ${kun}-${oy}-${yil} | Haftaning kuni: ${haftaKuni}`
 );
+
+
+
+
+let monthCount = [31,28,31,30,31,30,31,31,30,31,30,31]
+let data = new Date()
+function getDay(arr,d){
+  let daySum = 0
+  for(let i = 0; i< d.getMonth(); i++){
+    daySum += arr[i]
+  }
+  daySum += Number(d.toDateString().split(" ")[2])
+  return daySum
+}
+
+console.log(getDay(monthCount,date));
