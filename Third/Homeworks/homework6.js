33bdb5d12ff119436b05bc1233c45871caea4799
ZENTRIXXX <azizbee353@gmail.com>
2026-03-24 07:32:34 +0500

    homework 6

---
+++ Third/Homeworks/homework6.js
@@ -0,0 +1,31 @@
+function findAverage(array) {
+  if (array.length === 0) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum / array.length;
+}
+
+console.log(findAverage([1,1,1])); 
+console.log(findAverage([1,2,3])); 
+console.log(findAverage([1,2,3,4])); 
+
+
+console.log("======================");
+
+
+function sum(numbers) {
+  let total = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+
+  return total;
+}
+
+console.log(sum([])); // 0
+console.log(sum([1, 5.2, 4, 0, -1])); 
